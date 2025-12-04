3f8bf6ca76fd05ddbab9c8ac03e7f6fdac512db3
Margarita <marharitabykadorova@gmail.com>
2025-12-04 10:57:51 +0100

    Add DataManager class with full CRUD operations using SQLAlchemy ORM

---
+++ data_manager.py
@@ -0,0 +1,48 @@
+from models import db, User, Movie
+
+
+class DataManager:
+    """Data access layer for users and movies."""
+
+    def create_user(self, name):
+        """Add a new user to the database."""
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+    def get_users(self):
+        """Return a list of all users in the database."""
+        return User.query.all()
+
+    def get_movies(self, user_id):
+        """Return a list of all the movies for a specific user."""
+        return Movie.query.filter_by(user_id=user_id).all()
+
+    def add_movie(self, movie):
+        """
+        Add a new movie to a user's favorites.
+
+        Expects 'movie' to be a Movie instance that already has
+        its fields (name, director, year, poster_url, user_id) set.
+        """
+        db.session.add(movie)
+        db.session.commit()
+        return movie
+
+    def update_movie(self, movie_id, new_title):
+        """Update the title of a specific movie in the database."""
+        movie = Movie.query.get(movie_id)
+        if movie is None:
+            return None
+        movie.name = new_title
+        db.session.commit()
+        return movie
+
+    def delete_movie(self, movie_id):
+        """Delete a movie from the user's list of favorites."""
+        movie = Movie.query.get(movie_id)
+        if movie is None:
+            return
+        db.session.delete(movie)
+        db.session.commit()

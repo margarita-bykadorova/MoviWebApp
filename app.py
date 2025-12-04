c3501def9f9ed61f7997549b8dd8bc509dcaa572
Margarita <marharitabykadorova@gmail.com>
2025-12-04 13:23:52 +0100

    Add initial routes, base template, and stylesheet setup

--- app.py
+++ app.py
@@ -20,7 +20,47 @@ data_manager = DataManager()
 
 @app.route('/')
 def home():
+    """Show a list of all registered users and a form for adding new users."""
     return "Welcome to MoviWeb App!"
+
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    """Add the new user info to the database, then redirect back to the home page."""
+    pass
+
+
+@app.route('/users/<int:user_id>/movies', methods=['GET'])
+def get_user_movies(user_id):
+    """Display the user’s list of favorite movies."""
+    pass
+
+
+@app.route('/users/<int:user_id>/movies', methods=['POST'])
+def add_user_movie(user_id):
+    """
+    Add a new movie to a user’s list of favorite movies.
+      - read the movie title from the form
+      - fetch details from OMDb
+      - create a Movie instance
+      - save it via data_manager.add_movie(movie)
+    """
+    pass
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
+def update_movie(user_id, movie_id):
+    """
+    Modify the title of a specific movie in a user’s list,
+    without depending on OMDb for corrections.
+    """
+    pass
+
+
+@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+def delete_movie(user_id, movie_id):
+    """Remove a specific movie from a user’s favorite movie list."""
+    pass
 
 
 if __name__ == '__main__':
